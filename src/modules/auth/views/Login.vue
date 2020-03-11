f8cade0271700c6607cbc12eb88c2851af93ad63
Jessica <alves.bernan@gmail.com>
2020-03-11 20:38:04 -0300

    Adicionando validação login

--- src/modules/auth/views/Login.vue
+++ src/modules/auth/views/Login.vue
@@ -15,17 +15,24 @@
                 name="email"
                 label="Email"
                 type="email"
+                :error-messages="emailErrors"
+                :success="$v.user.email.$invalid"
+                v-model.trim="$v.user.email.$model"
               ></v-text-field>
               <v-text-field
                 prepend-icon="lock"
                 name="password"
                 label="Senha"
                 type="password"
+                :error-messages="passwordErros"
+                :success="$v.user.password.$invalid"
+                v-model.trim="$v.user.password.$model"
               ></v-text-field>
             </v-form>
             <v-btn
               block
               depressed
+              @click="log"
             >
               Criar Conta
             </v-btn>
@@ -35,8 +42,10 @@
           <v-card-actions>
             <v-spacer></v-spacer>
             <v-btn
+              :disabled="$v.$invalid"
               color="primary"
               large
+              @click="submit"
             >Login</v-btn>
           </v-card-actions>
         </v-card>
@@ -45,3 +54,58 @@
     </v-layout>
   </v-container>
 </template>
+
+<script>
+
+import { required, email, minLength } from 'vuelidate/lib/validators'
+
+export default {
+  name: 'Login',
+  data: () => ({
+    user: {
+      email: '',
+      password: ''
+    }
+  }),
+  validations: {
+    user: {
+      email: {
+        required,
+        email
+      },
+      password: {
+        required,
+        minLength: minLength(6)
+      }
+    }
+  },
+  computed: {
+    emailErrors () {
+      const errors = []
+      const email = this.$v.user.email
+      if (!email.$dirty) { return errors }
+      !email.required && errors.push('Email é obrigatório!')
+      !email.email && errors.push('Insira um email válido!')
+
+      return errors
+    },
+    passwordErros () {
+      const errors = []
+      const password = this.$v.user.password
+      if (!password.$dirty) { return errors }
+      !password.required && errors.push('Senha é obrigatória!')
+      !password.minLength && errors.push(`Insira pelo menos ${password.$params.minLength.min} caracteres!`)
+
+      return errors
+    }
+  },
+  methods: {
+    log () {
+      console.log('Vuelidade: ', this.$v)
+    },
+    submit () {
+      console.log('Vuelidade: ', this.user)
+    }
+  }
+}
+</script>
